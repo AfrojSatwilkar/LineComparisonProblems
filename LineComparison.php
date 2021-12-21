e09de3ab2c01aef92392b295ab6d3759123bfeec
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-12-21 16:46:19 +0530

    Checking two lines are equal or not

--- LineComparison.php
+++ LineComparison.php
@@ -6,6 +6,9 @@ class LineComparison {
     public $pointY1;
     public $pointY2;
 
+    /**
+     * getUserInput method is used to initialize variable
+	 */
     public function getUserInput() {
         $this->pointX1 = readline("Enter line X1 coordinates : ");
         $this->pointX2 = readline("Enter line X2 coordinates : ");
@@ -13,17 +16,41 @@ class LineComparison {
         $this->pointY2 = readline("Enter line Y2 coordinates : ");
     }
 
+    /**
+	 * lengthOfLine method is used to calculate length of line
+	 * @return distance
+	 */
     public function lengthOfLine() {
-        // using Math.pow and Math.sqrt method
+        // using pow and sqrt method
         $distance = sqrt(
                     (pow(($this->pointX2 - $this->pointX1), 2)) + 
                     (pow($this->pointY2 - $this->pointY1, 2)));
         return $distance;
 
     }
+
+    /**
+	 * checkEquality method is used to check lines equal or not
+	 * @param line1,line2
+	 */
+    public function checkEquality($line1, $line2) {
+        if ($line1 == $line2) {
+            echo "both line length are equal";
+        } else {
+            echo "both line length are different";
+        }
+    }
 }
 
 $calculateLength = new LineComparison();
 $calculateLength->getUserInput();
-echo "Length of line is : " . $calculateLength->lengthOfLine();
+$line1 = $calculateLength->lengthOfLine();
+echo "Length of line1 is : " . round($line1, 2) . "\n";
+
+$calculateLength->getUserInput();
+$line2 = $calculateLength->lengthOfLine();
+echo "Length of line2 is : " . round($line2, 2) . "\n";
+
+$calculateLength->checkEquality($line1, $line2);
+
 ?>
